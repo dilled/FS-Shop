33f784e685dd58b762fc2a4a88272b8ea0ebe771
dilled <hhuhtala0@gmail.com>
2020-05-19 07:50:36 +0300

    test

---
+++ frontend/build/precache-manifest.7b60d49cc4996800e5ba545b396606bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c331ec1727b7b73b51b0d82c7eec1212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240944521c3ac7bbc3b9",
+    "url": "/static/css/main.5a5c8383.chunk.css"
+  },
+  {
+    "revision": "60c4f721a698b28d7a7a",
+    "url": "/static/js/2.fac8f6a3.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.fac8f6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240944521c3ac7bbc3b9",
+    "url": "/static/js/main.9ba19eb2.chunk.js"
+  },
+  {
+    "revision": "922a203437e06ef547cb",
+    "url": "/static/js/runtime-main.88cdad98.js"
+  }
+]);
